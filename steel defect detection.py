e3e78f107a87562cb6008ea441bb7f4665231f93
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2021-12-24 18:54:00 -0500

    Plotting the test image

--- steel defect detection.py
+++ steel defect detection.py
@@ -3,4 +3,11 @@ import pickle
 import numpy as np
 import sklearn
 from imutils import paths
+import matplotlib.pyplot as plt
 
+TEST_PATH = '../Steel Defect Detection Dataset/test_images'
+TEST_IMAGE = TEST_PATH + '/0000f269f.jpg'
+
+plt.imread(TEST_IMAGE)
+plt.imshow(TEST_IMAGE)
+plt.show()

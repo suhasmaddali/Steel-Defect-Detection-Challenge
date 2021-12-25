7f36945bbc6a7d5ece4572bd5aa18d355ae365ab
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2021-12-24 21:07:02 -0500

    Executed the .py file with loading the model

--- steel defect detection.py
+++ steel defect detection.py
@@ -7,26 +7,22 @@ from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 
 TEST_PATH = '../Steel Defect Detection Dataset/test_images'
-TEST_IMAGE = TEST_PATH + '/0000f269f.jpg'
+TRAIN_PATH = '../Steel Defect Detection Dataset/train_images'
 count = 0
-for random_image in paths.list_images(TEST_PATH):
-    if count == 3:
+for random_image in paths.list_images(TRAIN_PATH):
+    if count == 0:
         img = cv2.imread(random_image, cv2.IMREAD_COLOR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (224, 224))
         img = img / 255
+        break
     count += 1
 
 img = np.array(img, dtype = np.float32)
-print(img)
-print(img.shape)
-img = np.reshape(img, (1, 224, 224, 3))
-pretrained_VGG19_model = load_model('Models/VGG19_weights.h5')
-print(pretrained_VGG19_model)
-print(pretrained_VGG19_model.summary())
-y_prediction = pretrained_VGG19_model.predict(img)
 
+img = img.reshape((1, 224, 224, 3))
+pretrained_VGG19_model = load_model('Models/VGG19_weights.h5')
+y_prediction = pretrained_VGG19_model.predict(img)
 print(y_prediction)
-#plt.imread(TEST_IMAGE)
-#plt.imshow(TEST_IMAGE)
-#plt.show()
+class_prediction = np.argmax(y_prediction) + 1
+print(class_prediction)

1e0120ca2605b10438128db060701fbd284cba3a
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2021-12-25 18:38:53 -0500

    Added the basic flask application

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
 
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug = True)

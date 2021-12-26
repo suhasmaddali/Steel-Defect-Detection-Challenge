0f8a5f6c066bec2245d9d27beecfb9d308acb8a0
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2021-12-26 15:49:40 -0500

    Added few .html changes:

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("base.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return "about page"
 
 if __name__ == "__main__":
     app.run(debug = True)

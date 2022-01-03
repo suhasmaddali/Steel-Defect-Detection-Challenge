2d8a43f5b8a46c78860d61d2856b1c17e08b1c30
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2022-01-02 21:47:54 -0500

    Made changes to app.py file

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('base.html')
 
 MODEL_PATH = 'Models/VGG19_weights.h5'
 
@@ -23,6 +20,28 @@ def model_prediction(img_location, model):
     prediction = model.predict(x)
     prediction = np.argmax(prediction, axis = 1)
     return prediction
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.jion(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        preds = model_prediction(file_path, model)
+        result = preds
+        return result
+    return None
+
+
+
 
 
 if __name__ == '__main__':

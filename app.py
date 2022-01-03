1eb408930c3087ea8198ac9a176c60f0157f0494
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2022-01-02 23:01:13 -0500

    Added a basic file

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-
+from flask import Flask, redirect, url_for, request, render_template
+import os
+from PIL import Image
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,34 +14,42 @@ MODEL_PATH = 'Models/VGG19_weights.h5'
 
 model = load_model(MODEL_PATH)
 
-def model_prediction(img_location, model):
-    img = image.load_img(img_location, target_size = (224, 224))
-    x = image.img_to_array(img)
-    x = x/255
-    x = np.expand_dims(x, axis = 0)
+def prepare_image(img):
+    img = Image.open(io.BytesIO(img))
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = np.expand_dims(img, 0)
+    return img
 
-    prediction = model.predict(x)
-    prediction = np.argmax(prediction, axis = 1)
-    return prediction
+def predict_result(img):
+    return np.argmax(model.predict(img), axis = 1)
 
+@app.route('/predict', methods = ['POST'])
+def infer_image():
+    if 'file' not in request.files:
+        return "Please try again. The Image doesn't exist"
 
+    file = request.files.get('file')
 
+    if not file:
+        return
 
-@app.route('/')
+    img_bytes = file.read()
+    img = prepare_image(img_bytes)
+    return jsonify(prediction = model_prediction(img_bytes))
+
+@app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
+    return 'Machine Learning Inference'
 
-@app.route('/predict', methods = ['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        f = request.files['file']
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.jion(basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-        preds = model_prediction(file_path, model)
-        result = preds
-        return result
-    return None
+
+
+
+
+
+
+
+
 
 
 

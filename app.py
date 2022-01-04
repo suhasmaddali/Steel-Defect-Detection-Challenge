49da532453c25ebacc5e47182a54c0d5122e5db4
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2022-01-03 20:45:20 -0500

    made small changes to the code

--- app.py
+++ app.py
@@ -9,20 +9,10 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET'])
+def index():
+    return 'Machine Learning Inference'
 
-MODEL_PATH = 'Models/VGG19_weights.h5'
-
-model = load_model(MODEL_PATH)
-
-def prepare_image(img):
-    img = Image.open(io.BytesIO(img))
-    img = img.resize((224, 224))
-    img = np.array(img)
-    img = np.expand_dims(img, 0)
-    return img
-
-def predict_result(img):
-    return np.argmax(model.predict(img), axis = 1)
 
 @app.route('/predict', methods = ['POST'])
 def infer_image():
@@ -37,23 +27,6 @@ def infer_image():
     img_bytes = file.read()
     img = prepare_image(img_bytes)
     return jsonify(prediction = model_prediction(img_bytes))
-
-@app.route('/', methods = ['GET'])
-def index():
-    return 'Machine Learning Inference'
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)

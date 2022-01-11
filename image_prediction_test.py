1eb61fa33b8a4378ceacb07a03a5a925fb775949
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2022-01-10 20:37:37 -0500

    Made changes in the code for web design

--- image_prediction_test.py
+++ image_prediction_test.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 TRAIN_IMAGE_PATH = '../Steel Defect Detection Dataset/train_images/00f6e702c.jpg'
 
 def model_predict(img_path, model):
-    img = image.load_img(TRAIN_IMAGE_PATH, target_size = (224, 224))
+    img = image.load_img(img_path, target_size = (224, 224))
     x = image.img_to_array(img)
     x = x / 255
     x = np.expand_dims(x, axis = 0)
@@ -28,13 +28,14 @@ random_output = model_predict(TRAIN_IMAGE_PATH, model)
 def upload():
     if request.method == 'POST':
         f = request.files["samplefile"]
-        print(f)
         basepath = os.path.dirname(__file__)
         print(basepath)
         file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        print(file_path)
+        f.save(file_path)
         predictions = model_predict(file_path, model)
         result = predictions
-        print(result)
+        #print(result)
         result = str(result)
         return result
     return render_template("index.html")

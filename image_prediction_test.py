4a64b73ea6863be345cd1cc1a9103703f9ccb61a
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2022-01-11 20:43:20 -0500

    Added javascript file

--- image_prediction_test.py
+++ image_prediction_test.py
@@ -8,12 +8,8 @@ from werkzeug.utils import secure_filename
 MODEL_PATH = 'Models/VGG19_weights.h5'
 
 model = load_model(MODEL_PATH)
-
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # model.summary()
+app = Flask(__name__)
 TRAIN_IMAGE_PATH = '../Steel Defect Detection Dataset/train_images/00f6e702c.jpg'
 
 def model_predict(img_path, model):
@@ -29,8 +25,13 @@ random_output = model_predict(TRAIN_IMAGE_PATH, model)
 
 #print(random_output)
 
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         # Get the file from post request
@@ -46,26 +47,7 @@ def upload():
         preds = model_predict(file_path, model)
         result=preds
         return result
-    return render_template('index.html')
-
-
-# @app.route('/', methods = ['GET', 'POST'])
-# def upload():
-#     if request.method == 'POST':
-#         f = request.files["file"]
-#         basepath = os.path.dirname(__file__)
-#         print(basepath)
-#         file_path = os.path.join(basepath, 'uploads')
-#         f.save(os.path.join(file_path, secure_filename(f.filename)))
-#         print(file_path)
-#         predictions = model_predict(file_path, model)
-#         result = predictions
-#         print(result)
-        # result = str(result)
-        # return result
-        # return render_template("index.html")
-
-
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True)
